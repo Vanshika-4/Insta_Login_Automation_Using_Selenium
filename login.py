4d2b15ae8c978da42dd336b6fb9b6caa89385c28
Vanshika <99902976+Vanshika-4@users.noreply.github.com>
2024-12-05 22:18:19 +0530

    Update login.py

--- login.py
+++ login.py
@@ -1,48 +1,3 @@
-# from selenium import webdriver
-# from selenium.webdriver.common.by import By
-# from selenium.webdriver.common.keys import Keys
-# from selenium.webdriver.chrome.service import Service
-# import time
-
-# options = Options()
-# options.add_argument("start-maximized")
-# options.add_argument("--disable-notifications")
-# options.add_experimental_option("excludeSwitches", ["enable-automation"])
-# options.add_experimental_option("useAutomationExtension", False)
-
-# service = Service("C:\\Windows\\WebDriver\\chromedriver.exe")
-# driver = webdriver.Chrome(service=service)
-# driver.get("https://www.instagram.com/")
-# time.sleep(5)
-
-# username_field = driver.find_element(By.XPATH, "//*[@id='loginForm']/div/div[1]/div/label/input")
-# password_field = driver.find_element(By.XPATH, "//*[@id='loginForm']/div/div[2]/div/label/input")
-
-# username_field.send_keys("vanshikasoumya22@gmail.com")
-# password_field.send_keys("Vansh@11")
-
-# time.sleep(5)
-
-# login_button = driver.find_element(By.XPATH, "//button[@type='submit']")
-# login_button.click()
-# time.sleep(5)
-
-# try:
-#     not_now_button = driver.find_element(By.XPATH, "//*[@id='mount_0_0_QB'']/div/div/div/div[2]/div/div/div[1]/div[1]/div[1]/section/main/div/div/div/div/div")
-#     not_now_button.click()
-#     time.sleep(2)
-# except Exception as e:
-#     print("No 'Not Now' button found:", e)
-# time.sleep(5)
-# try:
-#     notifications_button = driver.find_element(By.XPATH, "//*[@id='mount_0_0_QB'']/div/div/div/div[2]/div/div/div[1]/div[1]/div[1]/section/main/div/div/div/div/div")
-#     notifications_button.click()
-#     time.sleep(2)
-# except Exception as e:
-#     print("No notifications button found:", e)
-
-# time.sleep(5)
-
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
@@ -84,9 +39,9 @@ try:
         (By.CSS_SELECTOR, "input[name='password']")))
     
     username_field.clear()
-    username_field.send_keys("vanshikasoumya22@gmail.com")
+    username_field.send_keys("<__YOUR_PASSWORD_HERE__>")
     password_field.clear()
-    password_field.send_keys("Vansh@11")
+    password_field.send_keys("<__YOUR_PASSWORD_HERE__>") 
     
     # Click login button
     print("Clicking login button...")
